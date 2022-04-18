1ca15cd52b6bd04f65b589d0a180066cbfeee075
SaoDiSeng <1300808192@qq.com>
2022-04-18 20:34:57 +0800

    删除视频接口

--- service/service_vod/src/main/java/com/saodiseng/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/saodiseng/vod/controller/VodController.java
@@ -1,12 +1,14 @@
 package com.saodiseng.vod.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
 import com.saodiseng.commonutils.R;
+import com.saodiseng.servicebase.exceptionhandler.GuliException;
 import com.saodiseng.vod.service.VodService;
+import com.saodiseng.vod.utils.ConstantVodUtils;
+import com.saodiseng.vod.utils.InitVodClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -22,5 +24,23 @@ public class VodController {
         //返回视频id
         String videoId = vodService.uploadVideoAly(file);
         return R.ok().data("videoId",videoId);
+    }
+
+    //根据视频id删除阿里云视频
+    @DeleteMapping("removeAlyVideo/{id}")
+    public R removeAlyVideo(@PathVariable String id){
+        try{
+            //初始化对象
+            DefaultAcsClient client = InitVodClient.initVodClient(ConstantVodUtils.KEYID,ConstantVodUtils.KEYSECRET);
+            //创建删除视频的request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+            //像request设置id
+            request.setVideoIds(id);
+            client.getAcsResponse(request);
+            return R.ok();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new GuliException(20001,"删除视频失败");
+        }
     }
 }
