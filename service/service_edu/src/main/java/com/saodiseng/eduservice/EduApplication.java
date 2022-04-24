0a4ea4407d7439047d9037400613a67d4dd8f046
SaoDiSeng <1300808192@qq.com>
2022-04-24 15:33:56 +0800

    nacos注册客户端注解

--- service/service_edu/src/main/java/com/saodiseng/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/saodiseng/eduservice/EduApplication.java
@@ -4,8 +4,10 @@ package com.saodiseng.eduservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient // nacos注册客户端注解
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.saodiseng"})
 public class EduApplication {
