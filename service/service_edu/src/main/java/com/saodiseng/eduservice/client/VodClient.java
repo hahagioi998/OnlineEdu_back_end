43bebe2cf8a150981ec018d281607bb1ae14a817
SaoDiSeng <1300808192@qq.com>
2022-04-24 19:46:52 +0800

    删除多个视频调用端

--- service/service_edu/src/main/java/com/saodiseng/eduservice/client/VodClient.java
+++ service/service_edu/src/main/java/com/saodiseng/eduservice/client/VodClient.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Component  //spring管理
 @FeignClient("service-vod") //调用哪个服务
@@ -14,5 +17,10 @@ public interface VodClient {
     //根据视频id删除阿里云视频
     @DeleteMapping("/eduvod/video/removeAlyVideo/{id}")  //完全路径
     public R removeAlyVideo(@PathVariable("id") String id);
+
+    //删除多个阿里云视频
+    //参数是多个视频id
+    @DeleteMapping("/eduvod/video/delete-batch")
+    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList);
 
 }
