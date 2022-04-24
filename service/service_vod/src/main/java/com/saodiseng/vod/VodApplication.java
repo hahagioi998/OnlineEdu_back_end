0ba3c3dfc31555d0cf587ace554ebe50b570bb1e
SaoDiSeng <1300808192@qq.com>
2022-04-24 16:02:18 +0800

    nacos注解

--- service/service_vod/src/main/java/com/saodiseng/vod/VodApplication.java
+++ service/service_vod/src/main/java/com/saodiseng/vod/VodApplication.java
@@ -3,11 +3,13 @@ package com.saodiseng.vod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //()不加载数据库相关配置，
 @ComponentScan(basePackages = {"com.saodiseng"})
+@EnableDiscoveryClient
 public class VodApplication {
     public static void main(String[] args) {
         SpringApplication.run(VodApplication.class, args);
