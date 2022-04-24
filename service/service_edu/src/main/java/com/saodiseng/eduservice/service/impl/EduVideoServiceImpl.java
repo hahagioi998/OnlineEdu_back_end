bfadfb55e6d03a5878257b65daccaade2c014571
SaoDiSeng <1300808192@qq.com>
2022-04-24 19:46:19 +0800

    删除课程同时删除多个视频业务逻辑代码实现

--- service/service_edu/src/main/java/com/saodiseng/eduservice/service/impl/EduVideoServiceImpl.java
+++ service/service_edu/src/main/java/com/saodiseng/eduservice/service/impl/EduVideoServiceImpl.java
@@ -1,11 +1,17 @@
 package com.saodiseng.eduservice.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.saodiseng.eduservice.client.VodClient;
 import com.saodiseng.eduservice.entity.EduVideo;
 import com.saodiseng.eduservice.mapper.EduVideoMapper;
 import com.saodiseng.eduservice.service.EduVideoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -18,10 +24,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class EduVideoServiceImpl extends ServiceImpl<EduVideoMapper, EduVideo> implements EduVideoService {
 
+    //注入VodClient
+    @Autowired
+    private VodClient vodClient;
+
     //根据课程id删除小节
-    //TODO 删除小节，删除对应视频文件
+    //删除小节，删除对应视频文件
     @Override
     public void removeVideoByCourseId(String courseId) {
+        //根据课程id查询出所有视频id
+        QueryWrapper<EduVideo> wrapperVideo = new QueryWrapper<>();
+        wrapperVideo.eq("course_id",courseId);
+        wrapperVideo.select("video_source_id");
+        List<EduVideo> eduVideos = baseMapper.selectList(wrapperVideo);
+        //List<EduVideo>编程List<String>
+        List<String> videoIds = new ArrayList<>();
+        for (int i = 0; i < eduVideos.size(); i++) {
+            EduVideo eduVideo = eduVideos.get(i);
+            String videoSourceId = eduVideo.getVideoSourceId();
+            if(!StringUtils.isEmpty(videoSourceId)){
+                videoIds.add(videoSourceId);
+            }
+        }
+        if(videoIds.size() > 0){
+            vodClient.deleteBatch(videoIds);
+        }
         QueryWrapper<EduVideo> wrapper = new QueryWrapper<>();
         wrapper.eq("course_id",courseId);
         baseMapper.delete(wrapper);
